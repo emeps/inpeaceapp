2fd14753767593c3842c2665aecb8f652e039511
Emerson Patryck da Silva <emeps@outlook.com>
2022-09-05 01:38:43 -0300

    chore: change the name of the file to make sense of its function

--- public/assets/js/getStates.js
+++ public/assets/js/getStates.js
@@ -1,12 +1,8 @@
 const stateSelect = document.querySelector("select[name='estado']");
 
 fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-    .then((res) => {
-        return res.json();
-    })
-    .then((states) => {
-        buildOptionState(states)
-    });
+    .then(res => res.json())
+    .then(states => buildOptionState(states));
 
 function buildOptionState(states){
     for (state of states) {
